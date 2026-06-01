5737e171f7bab9aa189d7779a3ca5e4d0f4ac4f1
Dr. Sunder Ali Khowaja <sandar.ali@gmail.com>
2026-06-01 22:53:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
