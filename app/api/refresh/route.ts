5e125a01e9142488f8c3217f0b940050f416d6a8
Dr. Sunder Ali Khowaja <sandar.ali@gmail.com>
2026-06-01 22:34:13 +0100

    Update route.ts

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -2,6 +2,7 @@ import { scrapePermitData } from '@/lib/scraper';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
+  const CRON_SECRET = process.env.CRON_SECRET;
   const authHeader = request.headers.get('authorization');
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new NextResponse('Unauthorized', { status: 401 });
